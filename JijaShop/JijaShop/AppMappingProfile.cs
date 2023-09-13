51a7120079ec84ebd3f9b9609986f74024ff0c33
TrevorSomething675 <88676741+TrevorSomething675@users.noreply.github.com>
2023-09-13 17:31:41 +0300

    Нужно добавить дто для логина

--- JijaShop/JijaShop/AppMappingProfile.cs
+++ JijaShop/JijaShop/AppMappingProfile.cs
@@ -26,6 +26,7 @@ namespace JijaShop
 
             CreateMap<ProductDetails, ProductDetailsDto>().ReverseMap();
             CreateMap<User, UserDto>().ReverseMap();
+            CreateMap<User, LoginDto>().ReverseMap();
         }
     }
 }
