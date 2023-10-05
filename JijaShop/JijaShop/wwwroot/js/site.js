3496d8247709e6a12afec949e99aa70032b9aa21
IvanIvanovHome <soalone999@mail.ru>
2023-10-05 23:07:59 +0300

    c1

--- JijaShop/JijaShop/wwwroot/js/site.js
+++ JijaShop/JijaShop/wwwroot/js/site.js
@@ -1,17 +1,17 @@
-﻿$(document).ready(function () {
-    var counter = 0;
-$("#incrementButton").click(function () {
-    counter += 1;
-$.ajax({
-    url: 'Products',
-type: 'GET',
-data: { value: counter },
-success: function (result) {
-    $("#counterValue").text(counter);
-            },
-error: function (result) {
-    alert('error');
-            }
-        });
-    });
-});
+﻿//$(document).ready(function () {
+//    var counter = 0;
+//$("#incrementButton").click(function () {
+//    counter += 1;
+//$.ajax({
+//    url: 'Products',
+//type: 'GET',
+//data: { value: counter },
+//success: function (result) {
+//    $("#counterValue").text(counter);
+//            },
+//error: function (result) {
+//    alert('error');
+//            }
+//        });
+//    });
+//});
