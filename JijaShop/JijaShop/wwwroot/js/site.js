762444729541b22ab0fbe7b3d498c00e8507c221
TrevorSomething675 <88676741+TrevorSomething675@users.noreply.github.com>
2023-10-12 12:38:17 +0300

    Добавил липкий Sidebar и поисковую строку в него

--- JijaShop/JijaShop/wwwroot/js/site.js
+++ JijaShop/JijaShop/wwwroot/js/site.js
@@ -15,3 +15,23 @@
 //        });
 //    });
 //});
+
+$(document).ready(function () {
+    var headerSearch = $('#searchInHeader');
+    var sidebarSearch = $('#searchInSidebar');
+    var headerOffset = headerSearch.offset().top;
+
+    $(window).scroll(function () {
+        var scrollPos = $(window).scrollTop();
+
+        if (scrollPos >= headerOffset) {
+            headerSearch.removeClass('sticky-top');
+            headerSearch.addClass('fixed');
+            sidebarSearch.removeClass('d-none');
+        } else {
+            headerSearch.addClass('sticky-top');
+            headerSearch.removeClass('fixed');
+            sidebarSearch.addClass('d-none');
+        }
+    });
+});
